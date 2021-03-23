fff3e8a61dee63cc80e323f0fd33dc2e661f64e6
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-23 15:48:34 +0530

    Update __aifunctions.py

--- Elizabeth/modules/__aifunctions.py
+++ Elizabeth/modules/__aifunctions.py
@@ -3,8 +3,8 @@
 
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
-from MashaRoBot import telethn, OWNER_ID
-from MashaRoBot.events import register
+from Elizabeth import telethn, OWNER_ID
+from Elizabeth.events import register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
