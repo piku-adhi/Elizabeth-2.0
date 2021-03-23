5008ecf7d1858e16075badcb4e8a3552c2264e0e
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-23 20:23:33 +0530

    Update __aifunctions.py

--- Elizabeth/modules/__aifunctions.py
+++ Elizabeth/modules/__aifunctions.py
@@ -37,7 +37,7 @@ async def hmm(event):
    except CFError as e:
            print(e)
   elif "ann" in result:
-   pro = "My name is Masha"
+   pro = "My name is Elizabeth"
    try:
       async with client.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
