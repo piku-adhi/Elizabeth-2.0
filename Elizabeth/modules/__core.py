2e1f917da4924aee60d0a45e8dfd67e5bb9c631f
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-25 09:44:39 +0530

    Done

--- Elizabeth/modules/__core.py
+++ Elizabeth/modules/__core.py
@@ -36,7 +36,7 @@ async def Prof(event):
         await event.reply("No File Found!")
 
 
-from Elizabeth.events import LOAD_MODULES
+from Elizabeth.events import all_modules
 import asyncio
 import os
 from datetime import datetime
