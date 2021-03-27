2122cbb2de33150903c40e52ca98494e8131eedb
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-28 00:40:32 +0530

    Update events.py

--- Elizabeth/events.py
+++ Elizabeth/events.py
@@ -59,3 +59,63 @@ def callbackquery(**args):
         return func
 
     return decorator
+
+def masharobot(**args):
+    pattern = args.get("pattern", None)
+    disable_edited = args.get("disable_edited", False)
+    ignore_unsafe = args.get("ignore_unsafe", False)
+    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
+    group_only = args.get("group_only", False)
+    disable_errors = args.get("disable_errors", False)
+    insecure = args.get("insecure", False)
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    if "ignore_unsafe" in args:
+        del args["ignore_unsafe"]
+
+    if "group_only" in args:
+        del args["group_only"]
+
+    if "disable_errors" in args:
+        del args["disable_errors"]
+
+    if "insecure" in args:
+        del args["insecure"]
+
+    if pattern:
+        if not ignore_unsafe:
+            args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
+
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+        import MashaRoBot.events
+
+        path = Path(f"Elizabeth/modules/{shortname}.py")
+        name = "Elizabeth.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("Successfully imported " + shortname)
+    else:
+        import importlib
+        import Elizabeth.events
+
+        path = Path(f"Elizabeth/modules/{shortname}.py")
+        name = "Elizabeth.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.register = register
+        mod.masharobot = masharobot
+        mod.tbot = telethn
+        mod.logger = logging.getLogger(shortname)
+        spec.loader.exec_module(mod)
+        sys.modules["Elizabeth.modules." + shortname] = mod
+        print("Successfully imported " + shortname)
