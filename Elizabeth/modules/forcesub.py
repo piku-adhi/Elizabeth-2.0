293b70f6f63de3923f398f282d5ccf4836300d9e
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-29 13:09:33 +0530

    Update and rename __forcesub.py to forcesub.py

--- Elizabeth/modules/forcesub.py
+++ Elizabeth/modules/forcesub.py
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from MashaRoBot import DRAGONS as SUDO_USERS
-from MashaRoBot import pbot
-from MashaRoBot.modules.sql_extended import forceSubscribe_sql as sql
+from Elizabeth import DRAGONS as SUDO_USERS
+from Elizabeth import pbot
+from Elizabeth.modules.sql_extended import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
