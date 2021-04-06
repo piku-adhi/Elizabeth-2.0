7285f80035d672210f8297137da673ed9da9bc40
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-04-06 21:50:40 +0530

    Update upload.py

--- Elizabeth/modules/upload.py
+++ Elizabeth/modules/upload.py
@@ -181,10 +181,10 @@ if __name__ == "__main__":
     main()
 
 __help__ = """
- ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link
- ⦁ `/tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link
+ ➩ /transfersh: reply to a telegram file to upload it on transfersh and get direct download link
+ ➩ /tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link
 
-© @TheElizabethBot 👸
+
 """
 
-__mod_name__ = "File To Link 🔗"
+__mod_name__ = "File To Link"
